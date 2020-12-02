29015581266826e2834c8270f532fb56c4a63bde
Hedari <headrikun@gmail.com>
2020-12-02 23:24:52 +0300

    show simple window

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
+import { app, BrowserWindow } from "electron"
 
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+
+	if(process.env.IS_DEV == "true") {
+		win.loadURL("http://localhost:1234")
+	} else {
+		win.loadFile("./view/index.html")
+	}
+}
+
+app.whenReady().then(createWindow)
+
+app.on("window-all-closed", () => {
+	if(process.platform !== "darwin") {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if(BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+})
