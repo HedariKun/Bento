0d4793630d817ce209b2bd73e48d7e7a8eb84b52
Hedari <headrikun@gmail.com>
2020-12-03 03:34:33 +0300

    add moebooru api support

---
+++ src/views/api/moebooru.ts
@@ -0,0 +1,42 @@
+import {IAPI, IImageData } from "./interface"
+
+export default class MoeBooru implements IAPI { 
+	url: string = ""
+	page: number = 0
+
+	constructor(url: string) {
+		this.url = url
+	}
+
+	async getImages(tags: string, rating: string, page?: number, limit?: number): Promise<IImageData[]> {
+
+		let tagValue = tags.split(",").map(x=> x.replace(" ", "_")).join("+")
+		tagValue += `+rating:${rating}` 
+
+		const res = await fetch(this.url + `/post.json?tags=${tagValue}`)
+
+		const data = await res.json()
+		const images: IImageData[] = []
+		for (const image of data) {
+			images.push(
+				{
+					id: image.id,
+					tags: image.tags.split(" "),
+					createdAt: image.created_at,
+					updatedAt: image.updated_at,
+					rating: image.rating,
+					width: image.width,
+					height: image.height,
+					source: image.source,
+					fileSize: image.file_size,
+					imageUrl: image.image_url,
+					previewUrl: image.preview_url,
+					previewWidth: image.preview_width,
+					previewHeight: image.preview_height
+				}
+			)
+		}
+		return images
+	}
+}
+
--- src/views/script.tsx
+++ src/views/script.tsx
@@ -1,5 +1,7 @@
 import * as React from "react"
 import * as ReactDom from "react-dom"
+import 'regenerator-runtime/runtime'
+import MoeBooru from "./api/moebooru"
 import HomePage from "./homePage"
 
 ReactDom.render(HomePage(), document.getElementById("app"))
